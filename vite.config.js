e6dfdbb74564816a64b55d13750eb765746b9134
Amanv025 <108264087+Amanv025@users.noreply.github.com>
2024-07-28 22:22:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        // Add paths to external dependencies if needed
+        '/Edtech/edtech-project/fetchdata.js'
+      ]
+    }
+  }
 })
